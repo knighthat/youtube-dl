84d1209230c4230afefaa119d70ae35f8184659e
knighthat <knightchanelgaming@gmail.com>
2024-04-20 00:50:55 -0500

    use default message

--- src/main/java/me/knighthat/deps/exception/UnsupportedVersionException.java
+++ src/main/java/me/knighthat/deps/exception/UnsupportedVersionException.java
@@ -9,7 +9,7 @@ public class UnsupportedVersionException extends UnsupportedOperationException {
     @Serial
     private static final long serialVersionUID = 1857462938573627485L;
 
-    public UnsupportedVersionException( @NotNull String message ) {
-        super( message );
+    public UnsupportedVersionException( @NotNull String name, @NotNull String required ) {
+        super( "%s requires version %s or newer to run!".formatted( name, required ) );
     }
 }
