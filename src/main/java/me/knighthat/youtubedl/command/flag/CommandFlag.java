29faf02e26caaf16310b383cee97f80ce3a39dec
knighthat <knightchanelgaming@gmail.com>
2024-04-25 16:02:02 -0500

    allow CommandFlag creation without using lambda

--- src/main/java/me/knighthat/youtubedl/command/flag/CommandFlag.java
+++ src/main/java/me/knighthat/youtubedl/command/flag/CommandFlag.java
@@ -6,5 +6,30 @@ import java.util.Map;
 
 public interface CommandFlag {
 
+    static @NotNull Builder key( @NotNull String key ) { return new Builder( key ); }
+
+    static @NotNull CommandFlag noValue( @NotNull String key ) { return new Builder( key ).build(); }
+
     @NotNull Map<String, String> arguments();
+
+    class Builder {
+
+        private final String key;
+        private       String value;
+
+        private Builder( @NotNull String key ) {
+            this.key = key;
+            this.value = "";
+        }
+
+        public @NotNull CommandFlag value( @NotNull String value ) {
+            if ( !this.value.isBlank() )
+                throw new IllegalAccessError( "value already set!" );
+
+            this.value = value;
+            return build();
+        }
+
+        public @NotNull CommandFlag build() { return () -> Map.of( key, value ); }
+    }
 }
