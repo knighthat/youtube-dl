0b86ba3286aa8d1c3a7df3d4893b0b6fa3ff9c93
knighthat <knightchanelgaming@gmail.com>
2024-04-25 09:23:49 -0500

    a class represents subtitle file

---
+++ src/main/java/me/knighthat/youtubedl/response/subtitle/Subtitle.java
@@ -0,0 +1,32 @@
+package me.knighthat.youtubedl.response.subtitle;
+
+import me.knighthat.youtubedl.exception.UnsupportedSubtitleFormatException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+public record Subtitle( @NotNull String language, @NotNull Format format, boolean isAutomatic ) {
+
+    public @NotNull Locale locale() {
+        return new Locale( language );
+    }
+
+    public enum Format {
+        VTT,
+        TTML,
+        SRV3,
+        SRV2,
+        SRV1,
+        JSON3;
+
+        public static @NotNull Format match( @NotNull String value ) {
+            for (Format f : values())
+                if ( f.name().equalsIgnoreCase( value ) )
+                    return f;
+
+            throw new UnsupportedSubtitleFormatException( value );
+        }
+
+        public @NotNull String extension() { return name().toLowerCase(); }
+    }
+}
