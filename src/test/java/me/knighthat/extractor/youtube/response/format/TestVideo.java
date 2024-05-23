2e889fd8a491d8f1d0f69543f36b04af6fee6836
knighthat <knightchanelgaming@gmail.com>
2024-05-22 21:09:31 -0500

    youtube's video unitests

---
+++ src/test/java/me/knighthat/extractor/youtube/response/format/TestVideo.java
@@ -0,0 +1,133 @@
+package me.knighthat.extractor.youtube.response.format;
+
+import java.math.BigInteger;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import me.knighthat.youtubedl.response.formats.Format;
+
+public class TestVideo {
+
+    @NotNull
+    private static final Gson GSON = new Gson();
+
+    @NotNull
+    static final String VALID_FORMAT = """
+        {
+            "format_id": "394",
+            "url": "",
+            "source_preference": -1,
+            "quality": 0,
+            "language": null,
+            "language_preference": -1,
+            "preference": null,
+            "filesize": 1809891,
+            "format_note": "144p",
+            "fps": 24,
+            "height": 144,
+            "tbr": 60.269,
+            "width": 256,
+            "ext": "mp4",
+            "vcodec": "av01.0.00M.08",
+            "acodec": "none",
+            "container": "mp4_dash",
+            "protocol": "http_dash_segments",
+            "fragments": [
+                {
+                    "url": ""
+                }
+            ],
+            "format": "394 - 256x144 (144p)",
+            "http_headers": {}
+        }
+    """;
+
+    /* MISSING "fps" */
+    @NotNull
+    static final String INVALID_FORMAT = """
+        {
+            "format_id": "394",
+            "url": "",
+            "source_preference": -1,
+            "quality": 0,
+            "language": null,
+            "language_preference": -1,
+            "preference": null,
+            "filesize": 1809891,
+            "format_note": "144p",
+            "height": 144,
+            "tbr": 60.269,
+            "width": 256,
+            "ext": "mp4",
+            "vcodec": "av01.0.00M.08",
+            "acodec": "none",
+            "container": "mp4_dash",
+            "protocol": "http_dash_segments",
+            "fragments": [
+                {
+                    "url": ""
+                }
+            ],
+            "format": "394 - 256x144 (144p)",
+            "http_headers": {}
+        }
+    """;
+
+    @Test
+    void testValidJsonConstructor() {
+        JsonObject validJson = GSON.fromJson( VALID_FORMAT, JsonObject.class );
+        Assertions.assertDoesNotThrow( () -> new Video( validJson ) );
+
+        Video video = new Video( validJson );
+        Assertions.assertEquals( Format.Type.VIDEO_ONLY, video.type() );
+        Assertions.assertEquals( "394", video.code() );
+        Assertions.assertEquals( "mp4", video.extension() );
+        Assertions.assertEquals( 60, video.tbr() );
+        Assertions.assertEquals( "av01.0.00M.08", video.vCodec() );
+        Assertions.assertEquals( "144p", video.resolution() );
+        Assertions.assertEquals( 24f, video.fps() );
+        Assertions.assertEquals( BigInteger.valueOf( 1809891L ), video.size() );
+    }
+
+    @Test
+    void testInvalidJsonConstructor() {
+        JsonObject invalidJson = GSON.fromJson( INVALID_FORMAT, JsonObject.class );
+        Assertions.assertThrows( 
+            NullPointerException.class, 
+            () -> new Video(invalidJson)
+        );
+    }
+
+    @Test
+    void testValidArrayConctructor() {
+        @NotNull
+        final String[] VALID_ARRAY = {
+            "400", 
+            "mp4", 
+            "2560x1440", 
+            "1440p", 
+            "5314k", 
+            "mp4_dash container", 
+            "av01.0.12M.08", 
+            "24fps", 
+            "video only", 
+            "152.20MiB"
+        };
+        Assertions.assertDoesNotThrow( () -> new Video( VALID_ARRAY ));
+
+        Video video = new Video( VALID_ARRAY );
+        Assertions.assertEquals( Format.Type.VIDEO_ONLY, video.type() );
+        Assertions.assertEquals( "400", video.code() );
+        Assertions.assertEquals( "mp4", video.extension() );
+        Assertions.assertEquals( 5314, video.tbr() );
+        Assertions.assertEquals( "av01.0.12M.08", video.vCodec() );
+        Assertions.assertEquals( "1440p", video.resolution() );
+        Assertions.assertEquals( 24f, video.fps() );
+        Assertions.assertEquals( BigInteger.valueOf( 159593267L ), video.size() );
+    }
+}
