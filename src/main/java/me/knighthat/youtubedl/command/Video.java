49a5021d548defed76c2b422ad19982cbbc18413
knighthat <knightchanelgaming@gmail.com>
2024-05-27 10:03:33 -0500

    removed unused

--- src/main/java/me/knighthat/youtubedl/command/Video.java
+++ src/main/java/me/knighthat/youtubedl/command/Video.java
@@ -1,64 +1,16 @@
 package me.knighthat.youtubedl.command;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import me.knighthat.extractor.youtube.response.subtitle.DownloadableSubtitle;
-import me.knighthat.extractor.youtube.response.subtitle.Subtitle;
 import me.knighthat.youtubedl.command.flag.Flag;
 import me.knighthat.youtubedl.command.flag.GeoConfig;
 import me.knighthat.youtubedl.command.flag.Header;
 import me.knighthat.youtubedl.command.flag.UserAgent;
-import me.knighthat.youtubedl.exception.UnsupportedSubtitleFormatException;
-import me.knighthat.youtubedl.logging.Logger;
 import me.knighthat.youtubedl.response.OptionalResponse;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Level;
 
 public abstract class Video extends Command {
-
-    @NotNull
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat( "yyyyMMdd" );
-
-    private static @NotNull Set<DownloadableSubtitle> subtitleSet( @NotNull JsonObject json, boolean isAutomatic ) {
-        Set<DownloadableSubtitle> subtitles = new HashSet<>();
-
-        for (String language : json.keySet()) {
-            for (JsonElement formatKey : json.getAsJsonArray(language)) {
-                JsonObject jsonFormat = formatKey.getAsJsonObject();
-
-                try {   
-                    String ext = jsonFormat.get("ext").getAsString();
-                    Subtitle.Format format = Subtitle.Format.match(ext);
-
-                    subtitles.add(
-                        new DownloadableSubtitle() {
-                            @Override
-                            public @NotNull String language() { return language; }
-
-                            @Override
-                            public @NotNull Format format() { return format; }
-
-                            @Override
-                            public boolean isAutomatic() { return isAutomatic; }
-
-                            @Override
-                            public @NotNull String url() { return jsonFormat.get("url").getAsString(); }  
-                        }
-                    );
-                } catch (UnsupportedSubtitleFormatException e) {
-                    Logger.exception("failed to parse subtitle!", e, Level.WARNING);
-                }
-            }
-        }
-
-        return subtitles;
-    }
 
     protected Video( @NotNull String url, @NotNull Set<Flag> flags, @NotNull Set<Header> headers, @Nullable UserAgent userAgent, @Nullable GeoConfig geoConfig ) {
         super( url, flags, headers, userAgent, geoConfig );
